2b33bbad3bb1ad595b96f19c57faf652cc42da19
Byron1701 <84128616+Byron1701@users.noreply.github.com>
2026-09-26 16:35:09 +0100

    Add F3 Havok endian detection

--- source/src/Havok/HavokPackfileReader.h
+++ source/src/Havok/HavokPackfileReader.h
@@ -36,6 +36,8 @@ struct PackFile {
     std::string version_string;
     // Havok layout-rules byte 0x11: 0 = big-endian, 1 = little-endian.
     bool little_endian = false;
+    // Havok layout-rules byte 0x11: 0 = big-endian, 1 = little-endian.
+    bool little_endian = false;
 
     SectionHeader classnames_section;
     SectionHeader data_section;
